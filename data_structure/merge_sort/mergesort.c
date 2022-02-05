b7d3ec12da80e7db9baa546030e9019beac47c12
dhcryan <98611647+dhcryan@users.noreply.github.com>
2022-02-06 05:00:17 +0900

    Update mergesort.c

--- data_structure/merge_sort/mergesort.c
+++ data_structure/merge_sort/mergesort.c
@@ -24,25 +24,25 @@ void merge_sort(int list[], int left, int right)
         mid = (left + right) / 2;
         merge_sort(list, left, mid);
         merge_sort(list, mid + 1, right);
-        merge(list, left, mid, right); //? •? ¬?œ ?‘ê°œì˜ ë°°ì—´ ?•©ë³?
+        merge(list, left, mid, right); //
     }
     else
         return;
 }
 void merge(int list[], int left, int mid, int right)
-{
+{ //¼ø¼­ Á¤·ÄÇÏ´Â ¿ªÇÒ
     int i, j, k, l;
     i = left;
     k = left;
     j = mid + 1;
-    while (i <= mid && j <= right)
+    while (i <= mid && j <= right) // mid±âÁØÀ¸·Î ¼ø¼­ ¹è¿­(sorted¿¡ )
     {
         if (list[i] <= list[j])
             sorted[k++] = list[i++];
         else
             sorted[k++] = list[j++];
     }
-    if (i > mid) // mid?™¼ìª½ì?? ?‹¤ ì±„ì›Œ?„£??? ê²½ìš° ?‚˜ë¨¸ì???Š” mid?˜¤ë¥¸ìª½?—?„œ ?‹¤ ë³µì‚¬?•´?„œ sortedë°°ì—´?— ?„£?œ¼ë©´ë¨
+    if (i > mid) //
         for (l = j; l <= right; l++)
             sorted[k++] = list[l];
     else
