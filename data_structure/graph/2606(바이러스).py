18c73a659e40d0807d3b47e5a2acafe2a4fb410e
dhcryan <98611647+dhcryan@users.noreply.github.com>
2022-02-27 09:14:17 +0900

    Update 2606(바이러스).py
    passed

--- data_structure/graph/2606(바이러스).py
+++ data_structure/graph/2606(바이러스).py
@@ -1,1 +1,20 @@
+n=int(input())
+m=int(input())
+
+graph=[[]*n for _ in range(n+1)]
+visited=[False]*(n+1)
+for _ in range(m):
+    i,j=map(int,input().split())
+    graph[i].append(j)
+    graph[j].append(i)
+cnt=0
+def virus(start_v):
+    global cnt
+    visited[start_v]=True
+    for w in graph[start_v]:
+        if visited[w]==False:
+            cnt+=1
+            virus(w)
+    return cnt
+print(virus(1))
 
