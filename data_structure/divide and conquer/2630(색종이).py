2ab1f12d3e524ed0d18821e542bb232b19311589
dhcryan <98611647+dhcryan@users.noreply.github.com>
2022-03-04 10:14:02 +0900

    Update 2630(색종이).py
    divide and conquer
    return 의 중요성

--- data_structure/divide and conquer/2630(색종이).py
+++ data_structure/divide and conquer/2630(색종이).py
@@ -1,1 +1,27 @@
+import sys
+N = int(input())
+graph = [list(map(int, input().split())) for _ in range(N)]
+result = []
+white, blue = 0, 0
 
+
+def search(x, y, n):
+    global blue, white
+    color = graph[x][y]
+    for i in range(x, x+n):
+        for j in range(y, y+n):
+            if color != graph[i][j]: #시작하는 지점부터 일정범위까지 중에서 다른 색깔이 나타나면
+                search(x, y, n//2)
+                search(x, y+n//2, n//2)
+                search(x+n//2, y, n//2)
+                search(x+n//2, y+n//2, n//2)
+                return
+    if color == 0:
+        white += 1
+    elif color == 1:
+        blue += 1
+
+
+search(0, 0, N)
+print(white)
+print(blue)
