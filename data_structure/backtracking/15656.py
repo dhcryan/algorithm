11cff1a602aeb482b37a03e44882fce4863b68c7
dhcryan <dhc4003@gmail.com>
2022-08-09 11:03:06 +0900

    15656

---
+++ data_structure/backtracking/15656.py
@@ -0,0 +1,14 @@
+N,M=map(int,input().split())
+arr=list(map(int,input().split()))
+arr.sort()
+output=[]
+def dfs(start):
+    global output
+    if len(output)==M:
+        print(' '.join(map(str,output)))
+        return
+    for i in range(0,N):
+        output.append(arr[i])
+        dfs(i)
+        output.pop()
+dfs(0)
